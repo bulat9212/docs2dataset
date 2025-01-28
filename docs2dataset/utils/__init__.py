6c96a0671d24c84ff61c6fcc7b5c1d34cea283b7
Булат Нургатин <zimlin99@gmail.com>
2025-01-28 07:11:15 +0300

    minor fix

--- docs2dataset/utils/__init__.py
+++ docs2dataset/utils/__init__.py
@@ -1,6 +1,6 @@
-# utils/__init__.py
 from .file_info import FileInfo
 from .file_path_manager import FilePathManager
 from .image_manager import ImageManager
 from .file_utils import is_image_file, create_directory
 from .logging_utils import setup_logger
+from .save_run_params import save_run_params
